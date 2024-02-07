64c4faa6f02b326413484064560d14b23234d6a6
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-07 22:47:03 +0100

    created header log out

---
+++ src/components/header/HeaderComponent2.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+  
+<template>
+    <header>
+      <img src="../../assets/images/logoHeader.png" alt="">
+      <div class="buttons">
+        <v-btn class="button1" color="yellow-darken-4">inicio</v-btn>
+        <v-btn class="button2" color="yellow-darken-4">cerrar sesión</v-btn>
+      </div>
+    </header> 
+   
+</template> 
+
+<style lang="scss" scoped>
+ header {
+   background-color: #7DC6D6;
+   width: 100%;
+   height: 150px;
+   display: flex;
+   justify-content: space-between;
+  }
+
+  .buttons {
+    margin-top: 80px;
+    margin-right: 50px;
+  }
+
+  .button1 {
+    margin-right: 25px;
+  }
+
+</style>
+
