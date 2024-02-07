6a7cb46c72285979113bcb3f6546199dfcff649b
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-07 20:54:38 +0100

    added changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup>
 import FooterComponent from '@/components/header-footer/FooterComponent.vue'
+import AddNewEvent from '@/components/AdminDashboard/AddNewEvent.vue'
 </script>
 
 <template>
   <main>
     <h1>Home View</h1>
-
+  <AddNewEvent />
   <FooterComponent />
   </main>
 </template>
