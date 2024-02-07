c24be20bf837eb0ab159c4122f9086eb6ab0d26f
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-07 22:45:59 +0100

    created header log in

---
+++ src/components/header/HeaderComponent.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+  
+<template>
+    <header>
+      <img src="../../assets/images/logoHeader.png" alt="">
+      <div class="buttons">
+        <v-btn class="button1" color="yellow-darken-4">Iniciar sesión</v-btn>
+        <v-btn class="button2" color="yellow-darken-4">registrarse</v-btn>
+      </div>
+    </header> 
+   
+</template> 
+
+<style lang="scss" scoped>
+ header {
+   background-color: #7DC6D6;
+   width: 100%;
+   height: 150px;
+   display: flex;
+   justify-content: space-between;
+  }
+
+  .buttons {
+    margin-top: 80px;
+    margin-right: 50px;
+  }
+
+  .button1 {
+    margin-right: 25px;
+  }
+
+</style>
