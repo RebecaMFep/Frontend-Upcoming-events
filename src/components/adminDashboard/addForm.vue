3a6399e24c059daf2bba4f42ad8a32e038905bab
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-07 23:03:38 +0100

    added changes addForm

--- src/components/adminDashboard/addForm.vue
+++ src/components/adminDashboard/addForm.vue
@@ -3,9 +3,22 @@
 </script>
 
 <template>
-    <div>
-        
-    </div>
+    <div class="login-box">
+
+<v-sheet class="mx-auto rounded-lg login-box" style="">
+  <div class="form">
+    <v-form fast-fail @submit.prevent="login()">
+      <v-text-field class="rounded-lg" v-model="username" label="Usuario" :rules="userRules"></v-text-field>
+
+      <v-text-field class="rounded-lg" v-model="password" label="Contraseña" :rules="passwordRules"></v-text-field>
+
+      <v-btn type="submit" color="orange" block class="mt-4 rounded-lg">Iniciar sesión</v-btn>
+
+      <v-btn type="" color="orange" block class="mt-4 rounded-lg">Registrarse</v-btn>
+    </v-form>
+  </div>
+</v-sheet>
+</div>
 </template>
 
 <style>
