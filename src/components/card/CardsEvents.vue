049d775a2617921fbc5a4538926f5bced131f02d
Brusmel Gil <Brusmel@outlook.es>
2024-02-12 12:34:30 +0100

    cards

---
+++ src/components/card/CardsEvents.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+const props = defineProps({
+    event: {
+        type: Object,
+        required: true,
+    },
+
+});
+
+</script>
+
+<template>
+
+    <v-card
+    class="mx-auto my-12"
+    max-width="300" :style="{ borderRadius: '15px', border: '2px solid blue'  }">
+    
+<!--     <v-img cover height="200" src="http://localhost:8080/images/roma.jpg "></v-img> -->
+<v-img
+cover
+height="200"
+width="300"
+src="http://localhost:8080/images/roma.jpg"
+></v-img>
+
+    <v-card-item> 
+        <v-card-title>{{ event.title }}</v-card-title>
+    </v-card-item>
+
+    <v-card-text>
+        <div class="my-4 text-subtitle-1">{{ event.date }}</div>
+        <div>{{ event.description }}</div>
+    </v-card-text>
+
+    <v-card-title>{{ event.place }}</v-card-title>
+    <v-card-title>{{ event.hour }}</v-card-title>
+
+<v-btn color="orange" absolute :style="{left: '50%', transform:'translateX(-50%)'}" class="mb-4">Inscribirme</v-btn>
+
+</v-card>
+
+
+</template>
+
+<style scoped lang="scss">
+
+
+</style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,39 @@
 <script setup>
 import { useEventStore } from "@/stores/event";
+import CardsEvents  from "@/components/card/CardsEvents.vue";
 
 const store = useEventStore()
 store.getEvents()
 
+
 </script>
 
 <template>
+
   <main>
-    <div v-for="(event) in store.events">
-      <v-card :title='event.title' 
-      :subtitle="event.id" 
-      :text="event.description"></v-card>
-    </div>
+<div v-for="event in store.events">
+  <CardsEvents :event="event" />
+
+</div>
+
+    
   </main>
 </template>
+
+<style scoped>
+
+main {
+  display: flex;
+  justify-content: space-around;
+  margin: 5%;
+  flex-wrap: wrap;
+  gap:  10px; 
+
+}
+
+@media (max-width:  768px) {
+  .CardsEvents {
+    width:  100%;
+  }
+}
+</style>
