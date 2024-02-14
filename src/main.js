3312d0e8915c8b18b5ea47f6453580018ca63a54
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-14 15:30:54 +0100

    created modify button, delete button and create event, backend call missing

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-import dayjs from 'dayjs'
-import DayJsAdapter from '@date-io/dayjs'
+// import dayjs from 'dayjs'
+// import DayJsAdapter from '@date-io/dayjs'
 
 
 const app = createApp(App)
@@ -19,7 +19,7 @@ const app = createApp(App)
 const vuetify = createVuetify({
   components,
   directives,
-  adapter: new DayJsAdapter(dayjs),
+  // adapter: new DayJsAdapter(dayjs),
 })
 
 app.use(createPinia())
