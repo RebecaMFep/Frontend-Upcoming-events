2892c0abda24459076ec476f036258036ad6d39c
Brusmel Gil <Brusmel@outlook.es>
2024-02-14 12:40:02 +0100

    card finished

--- src/components/card/CardsEvents.vue
+++ src/components/card/CardsEvents.vue
@@ -14,29 +14,33 @@ const props = defineProps({
 
     <v-card
     class="mx-auto my-12"
-    max-width="300" :style="{ borderRadius: '15px', border: '2px solid blue'  }">
+    max-width="250" :style="{ borderRadius: '15px', border: '2px solid blue', height: '440px'  }">
     
 <!--     <v-img cover height="200" src="http://localhost:8080/images/roma.jpg "></v-img> -->
 <v-img
 cover
-height="200"
+height="150"
 width="300"
-src="http://localhost:8080/images/roma.jpg"
-></v-img>
+src="http://localhost:8080/images/roma.jpg"></v-img>
 
-    <v-card-item> 
+    <v-card-item class="prueba"> 
         <v-card-title>{{ event.title }}</v-card-title>
     </v-card-item>
 
     <v-card-text>
-        <div class="my-4 text-subtitle-1">{{ event.date }}</div>
+        <div class="my-2 text-subtitle-1">{{ event.date }}</div>
         <div>{{ event.description }}</div>
     </v-card-text>
 
+<div class="evento">
     <v-card-title>{{ event.place }}</v-card-title>
     <v-card-title>{{ event.hour }}</v-card-title>
+</div>
 
-<v-btn color="orange" absolute :style="{left: '50%', transform:'translateX(-50%)'}" class="mb-4">Inscribirme</v-btn>
+
+<v-btn
+color="orange" absolute :style="{ left: '50%', transform: 'translateX(-50%)', width: '100px', fontSize: 'x-small' }" class="mb-2">Inscribirme
+</v-btn>
 
 </v-card>
 
@@ -44,6 +48,15 @@ src="http://localhost:8080/images/roma.jpg"
 </template>
 
 <style scoped lang="scss">
+    .prueba{
+        padding: 0;
+        margin-left: 5%;
+    }
+    .evento{
+        display: flex;
+        justify-content: space-between;
+    }
+
 
 
 </style>
