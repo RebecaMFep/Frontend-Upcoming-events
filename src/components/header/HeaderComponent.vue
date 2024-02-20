b041112179e3792731bfe64e21395f13685ead24
Ana Garcia Lopez <angarlopez@hotmail.com>
2024-02-20 15:41:29 +0100

    nuevos botones en el header

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -4,16 +4,29 @@ import { useRoute, useRouter } from "vue-router";
 const route = useRoute()
 const router = useRouter()
 
-const isLoginPage = route.path === '/login';
+const isLoggedIn = false; 
+const isLoginPage = router.currentRoute.value.name === 'login'; 
+
+const toggleAuth = () => {
+  if (isLoggedIn) {
+    logout();
+  } else {
+    goToLogin();
+  }
+}
+
 
 const goToLogin = () => {
   router.push('/login');
-};
+}
+
+const logout = () => {
+  // Hola soy Ana, se que falta código script aquí, pero si lo pongo se pone la pantalla en blanca, falta lógica del log out que no la he conseguido sacar. Mucha suerte chicos, y un placer haber estado estas semanas con vosotros. El team 4 lo va a petar!!!
+}
 
 const goToHome = () => {
   router.push('/');
-};
-
+}
 </script>
   
 <template>
@@ -21,12 +34,17 @@ const goToHome = () => {
       <img src="../../assets/images/icons/logoHeader.png" alt="">
       
       <div class="buttons">
-        <v-btn class="button1" color="orange-darken-1" v-if="!isLoginPage" @click="goToLogin">Iniciar sesión</v-btn>
-        <v-btn class="button1" color="orange-darken-1" v-if="isLoginPage" @click="goToHome">{{ showFullText ? 'Show less' : 'Show more' }}</v-btn>
+
+    
+
+        <v-btn class="button1" color="orange-darken-1" @click="toggleAuth">
+        {{ isLoggedIn ? 'Cerrar Sesión' : 'Iniciar Sesión' }}
+      </v-btn>
+        <v-btn class="button1" color="orange-darken-1" v-if="isLoggedIn && !isLoginPage" @click="goToHome">Home</v-btn>
         <v-btn class="button2" color="orange-darken-1">registrarse</v-btn>
+
       </div>
     </header> 
-   
 </template> 
 
 <style lang="scss" scoped>
