88d0592a2a2da1c07f5075e74bf9307b51f6c13d
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-25 13:11:00 +0100

    added variables.css

---
+++ src/components/header/HeaderComponent2.vue
@@ -0,0 +1,37 @@
+<script>
+
+
+</script>
+  
+<template>
+    <header>
+      <img src="../../assets/images/icons/logoHeader.png" alt="">
+      
+      <div class="buttons">
+        <v-btn class="button1" color="orange-darken-1">Inicio</v-btn>
+        <v-btn class="button2" color="orange-darken-1">Cerrar sesión</v-btn>
+      </div>
+    </header> 
+   
+</template> 
+
+<style lang="scss" scoped>
+ header {
+   background-color:#80CBC4;
+   width: 100%;
+   height: 150px;
+   display: flex;
+   justify-content: space-between;
+  }
+
+  .buttons {
+    margin-top: 80px;
+    margin-right: 50px;
+  }
+
+  .button1 {
+    margin-right: 25px;
+  }
+
+</style>
+
