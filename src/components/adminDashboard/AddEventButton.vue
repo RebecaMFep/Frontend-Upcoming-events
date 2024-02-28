afe41bafd96c01c80c75cd48fd5405d1126698ba
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-28 15:49:17 +0100

    added route

--- src/components/adminDashboard/AddEventButton.vue
+++ src/components/adminDashboard/AddEventButton.vue
@@ -4,16 +4,19 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 const navigateToForm = () => {
-  console.log('clicked');
-  router.push('/add-event') // Reemplaza '/formulario' por la ruta a la que quieres navegar
-}
+  router.push('/add-event');
+};
 </script>
 
 <template>
-    <v-btn color="orange-darken-1" id="send" :color="orange" @click="navigateToForm">Anadir nuevo evento</v-btn>
+    <v-btn class="mr-4" color="orange-darken-1" id="send" @click="navigateToForm">Anadir nuevo evento</v-btn>
 </template>
 
 <style lang="scss" scoped>
+
+.button {
+
+}
 
 </style>
     
