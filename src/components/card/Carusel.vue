3e936a508243b3c3e965b91a918a71bf43629a5a
Brusmel Gil <Brusmel@outlook.es>
2024-02-28 10:19:01 +0100

    g

--- src/components/card/Carusel.vue
+++ src/components/card/Carusel.vue
@@ -1,5 +1,6 @@
 <script setup>
 
+import { defineProps } from 'vue';
 const props = defineProps({
     events: {
         type: Array,
@@ -37,13 +38,12 @@ const items = [
 
 
 
-<template>
+<template  >
     <v-carousel>
     <v-carousel-item
-        v-for="(item,i) in items"
-        v-if="item.isOutstanding == true"
+    v-for="(item,i) in items"
         :key="i"
-        :src="item.src" 
+        :src="item.src"
     ></v-carousel-item>
     </v-carousel>
 </template>
